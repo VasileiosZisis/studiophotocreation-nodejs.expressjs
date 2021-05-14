b2b7723eb11a9dacabc74efc8d4d61196377ad45
GoneAway <vasil.zisis@gmail.com>
2021-05-14 20:11:36 +0300

    no message

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '.splide' , {
+        autoplay  : true,
+        interval  : 3000,
+        speed     : 800,
         type      : 'loop',
         perPage   : 3,
+        perMove   : 1,
         height    : '600px',
 	    cover     : true,
         arrows    : false,
         breakpoints: {
             640: {
-                perPage: 2,
+                perPage: 1,
             },
         }
     }).mount();
