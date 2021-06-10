cd8025df403b522374704289a59ee5a3541b77b0
GoneAway <vasil.zisis@gmail.com>
2021-06-10 19:16:08 +0300

    no message

--- main.js
+++ main.js
@@ -7,6 +7,31 @@ const modalImage = document.querySelector('.modal-content');
 const close = document.querySelector('.close');
 const modalIMG = document.querySelectorAll('.modal-IMG');
 
+// TRANSITION
+window.onload = () => {
+  const transitionEle = document.querySelector('.transition');
+  const anchors = document.querySelectorAll('a');
+
+  setTimeout (() => {
+    transitionEle.classList.remove('is-active');
+  }, 500);
+
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      transitionEle.classList.add('is-active');
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500);
+    });
+  }
+}
+
 // TEXT ANIMATION
 observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
