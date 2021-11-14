162e2195d1e9f9f4a35b236877953f26428a4374
GoneAway <vasil.zisis@gmail.com>
2021-11-14 22:22:13 +0200

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ cloudinary.config ({
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
-// app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use(helmet({contentSecurityPolicy: false,}));
 
 app.set('view engine', 'ejs');
@@ -43,6 +43,15 @@ app.get('/portfolio/english-weddings', (req, res) => {
 })
 app.get('/portfolio/wedding-stories', (req, res) => {
     res.render('./portfolio/wedding-stories')
+})
+app.get('/portfolio/proposals', (req, res) => {
+    res.render('./portfolio/proposals')
+})
+app.get('/portfolio/families', (req, res) => {
+    res.render('./portfolio/families')
+})
+app.get('/portfolio/models', (req, res) => {
+    res.render('./portfolio/models')
 })
 //christenings
 app.get('/portfolio/christenings/agapi', (req, res) => {
@@ -131,6 +140,78 @@ app.get('/portfolio/wedding-stories/lisa-darren', (req, res) => {
 })
 app.get('/portfolio/wedding-stories/stacey-aaron', (req, res) => {
     res.render('././portfolio/wedding-stories/stacey-aaron')
+})
+//proposals
+app.get('/portfolio/proposals/andreas-athina', (req, res) => {
+    res.render('././portfolio/proposals/andreas-athina')
+})
+app.get('/portfolio/proposals/chirag-priyanka', (req, res) => {
+    res.render('././portfolio/proposals/chirag-priyanka')
+})
+//families
+app.get('/portfolio/families/alex-gianna', (req, res) => {
+    res.render('././portfolio/families/alex-gianna')
+})
+app.get('/portfolio/families/alex-xenia', (req, res) => {
+    res.render('././portfolio/families/alex-xenia')
+})
+app.get('/portfolio/families/alexander-yiana', (req, res) => {
+    res.render('././portfolio/families/alexander-yiana')
+})
+app.get('/portfolio/families/angelika-misha', (req, res) => {
+    res.render('././portfolio/families/angelika-misha')
+})
+    app.get('/portfolio/families/baskhkim-shkourte', (req, res) => {
+    res.render('././portfolio/families/baskhkim-shkourte')
+})
+    app.get('/portfolio/families/filippos-emanouela', (req, res) => {
+    res.render('././portfolio/families/filippos-emanouela')
+})
+    app.get('/portfolio/families/galina-lionid', (req, res) => {
+    res.render('././portfolio/families/galina-lionid')
+})
+    app.get('/portfolio/families/ioulia-alexey', (req, res) => {
+    res.render('././portfolio/families/ioulia-alexey')
+})
+    app.get('/portfolio/families/julia-sofia', (req, res) => {
+    res.render('././portfolio/families/julia-sofia')
+})
+    app.get('/portfolio/families/maria-natalie', (req, res) => {
+    res.render('././portfolio/families/maria-natalie')
+})
+    app.get('/portfolio/families/rianna-ralf', (req, res) => {
+    res.render('././portfolio/families/rianna-ralf')
+})
+    app.get('/portfolio/families/rouslan-irina', (req, res) => {
+    res.render('././portfolio/families/rouslan-irina')
+})
+    app.get('/portfolio/families/sofie-ioulia', (req, res) => {
+    res.render('././portfolio/families/sofie-ioulia')
+})
+//models
+app.get('/portfolio/models/anna', (req, res) => {
+    res.render('././portfolio/models/anna')
+})
+app.get('/portfolio/models/d-and-d', (req, res) => {
+    res.render('././portfolio/models/d-and-d')
+})
+app.get('/portfolio/models/elena', (req, res) => {
+    res.render('././portfolio/models/elena')
+})
+app.get('/portfolio/models/johanna', (req, res) => {
+    res.render('././portfolio/models/johanna')
+})
+app.get('/portfolio/models/karlin', (req, res) => {
+    res.render('././portfolio/models/karlin')
+})
+app.get('/portfolio/models/natalie', (req, res) => {
+    res.render('././portfolio/models/natalie')
+})
+app.get('/portfolio/models/olga', (req, res) => {
+    res.render('././portfolio/models/olga')
+})
+app.get('/portfolio/models/sveta', (req, res) => {
+    res.render('././portfolio/models/sveta')
 })
 
 const port = process.env.PORT || 3000;
